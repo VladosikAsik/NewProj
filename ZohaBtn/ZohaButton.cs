ba07970ab5f1fbd55e06dd85cc1c37cd85e00764
Vlad <vladasekretov@gmail.com>
2021-07-06 10:51:47 +0300

    simple rect

--- ZohaBtn/ZohaButton.cs
+++ ZohaBtn/ZohaButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,20 @@ namespace ZohaBtn
 {
     class ZohaButton : Control
     {
+        //private Panel panel;
 
+        public ZohaButton() : base()
+        {
+            this.Size = new System.Drawing.Size(50, 50);
+            this.BackColor = Color.Gray;
+            this.Location = new Point(50, 50);
+            base.Paint += ZohaButton_Paint;
+        }
+
+        private void ZohaButton_Paint(object sender, PaintEventArgs e)
+        {
+            var g = CreateGraphics();
+            g.DrawRectangle(new Pen(Color.Red, 3), new Rectangle(50, 50, 50, 50));
+        }
     }
 }
