75c67f02a18debb17695148af6eb5482903b45c7
Vlad <vladasekretov@gmail.com>
2021-07-06 11:40:06 +0300

    maybe end

--- ZohaBtn/ZohaButton.cs
+++ ZohaBtn/ZohaButton.cs
@@ -10,20 +10,47 @@ namespace ZohaBtn
 {
     class ZohaButton : Control
     {
+        private bool check;
+        private Point mousePosition;
+        private Rectangle clickZone;
+
         public ZohaButton() : base()
         {
             this.Size = new System.Drawing.Size(200, 100);
             this.BackColor = Color.Purple;
             this.Location = new Point(100, 100);
             base.Paint += ZohaButton_Paint;
+            base.MouseClick += ZohaButton_MouseClick;
+            clickZone = new Rectangle(50, 35, 100, 30);
+        }
+
+        private void ZohaButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.mousePosition = new Point(e.Location.X, e.Location.Y);
+            Invalidate();
         }
 
         private void ZohaButton_Paint(object sender, PaintEventArgs e)
         {
             var g = CreateGraphics();
             var brush = new SolidBrush(Color.Blue);
-            g.FillRectangle(Brushes.Red, new RectangleF(50, 35, 100, 30));
-            g.FillEllipse(brush, new RectangleF(50, 35, 30, 30));
+            g.FillRectangle(Brushes.White, new RectangleF(50, 35, 100, 30));
+            check = true;
+
+            if(new Rectangle(mousePosition, new Size(1,1)).IntersectsWith(clickZone))
+            {
+                check = !check;
+            }
+            if (check == true)
+            {
+                g.FillRectangle(Brushes.Green, new RectangleF(50, 35, 100, 30));
+                g.FillEllipse(brush, new RectangleF(50, 35, 30, 30));
+            }
+            else if (check == false)
+            {
+                g.FillRectangle(Brushes.Red, new RectangleF(50, 35, 100, 30));
+                g.FillEllipse(brush, new RectangleF(120, 35, 30, 30));
+            }
         }
     }
 }
